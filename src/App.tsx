29e90251e19b9e1386ca78a057f5b23f66246900
Jared Goldstein <jegoldstein94@gmail.com>
2023-06-30 16:58:04 +1000

    feat: add some pretty icons and colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,56 @@
-import { Button, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Text } from "@chakra-ui/react";
+import { BiCoin } from "react-icons/bi";
 import { useState } from "react";
 
 function App() {
-  const [fishCount, setFishCount] = useState(0);
+  const [coins, setCoins] = useState(0);
+  const [clickValue, setClickValue] = useState(1);
+  const [upgradeCost, setUpgradeCost] = useState(25);
 
-  const addFish = () => {
-    setFishCount(fishCount + 1);
+  const addCoin = () => {
+    setCoins(coins + clickValue);
+  };
+
+  const upgradeClick = () => {
+    if (coins >= upgradeCost) {
+      setCoins(coins - upgradeCost);
+      setClickValue(clickValue + 1);
+      setUpgradeCost(Math.round(upgradeCost * (clickValue * 1.2)));
+    }
+  };
+
+  const bigUpgrade = () => {
+    if (coins >= 1000) {
+      setClickValue(clickValue + 15);
+      setCoins(coins - 1000);
+    }
   };
 
   return (
-    <div>
-      <h1>Idle Game</h1>
-      <Button onClick={addFish}>Fish</Button>
-      <Text>Fish: {fishCount}</Text>
-    </div>
+    <Box p="4">
+      <Heading>Idle Game</Heading>
+      <Flex direction="column">
+        <Text>Coint: {coins}</Text>
+        <Text>Click Value: {clickValue}</Text>
+        <Button
+          rightIcon={<BiCoin />}
+          leftIcon={<BiCoin />}
+          maxW="40"
+          colorScheme="purple"
+          onClick={addCoin}
+        >
+          Gather Coins
+        </Button>
+        <Text>Upgrade cost: {upgradeCost}</Text>
+        <Button maxW="36" colorScheme="yellow" onClick={upgradeClick}>
+          Upgrade Coins
+        </Button>
+        <Text>Big Upgrade cost: 1000</Text>
+        <Button maxW="36" colorScheme="yellow" onClick={bigUpgrade}>
+          Upgrade Coins
+        </Button>
+      </Flex>
+    </Box>
   );
 }
 
